06327087a5f36420acbaa923d0cd217a97986ca6
neto-works <clodoaldo.brtp4@gmail.com>
2024-03-17 09:56:49 -0300

    develop: implemented blog post writing part

---
+++ apps/post/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Post
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['titulo','descricao','likes','imagem','blogueiro_id']
+        widgets = {
+            'descricao': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),  # Defina a classe CSS e as propriedades desejadas
+        }
