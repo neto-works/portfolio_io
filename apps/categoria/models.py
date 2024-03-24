193aac4e65c7b6cfaace27f6ea305ca46e6ea24d
neto-works <clodoaldo.brtp4@gmail.com>
2024-03-24 23:41:46 +0000

    Committing local changes before merge

--- apps/categoria/models.py
+++ apps/categoria/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Categoria(models.Model):
     titulo = models.CharField(max_length=50, blank=True, null=True)
-    posters = models.ManyToManyField("post.Post", related_name="categorias", blank=True)
+    posters = models.ManyToManyField("post.Post", related_name="categorias",blank=True)
 
     def __str__(self):
         return str(self.titulo)
