865839e72325953b852ee3e9999f77581ecef06e
neto-works <clodoaldo.brtp4@gmail.com>
2024-04-02 15:10:41 -0300

    dev: install filter by title

--- apps/post/urls.py
+++ apps/post/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import blog_view, AHPView
+from .views import blog_view, AHPView, search
 
 urlpatterns = [
     path("", blog_view, name="blog"),
     path("ahp/", AHPView.as_view(), name="ahp"),
+    path("search/", search, name="search"),
 ]
--- apps/post/views.py
+++ apps/post/views.py
@@ -5,6 +5,7 @@ from .models import Post
 from django.core.paginator import Paginator
 from apps.categoria.models import Categoria
 from .utils.meu import AHP
+from django.http import Http404
 
 
 def blog_view(request):
@@ -44,6 +45,35 @@ def blog_view(request):
         },
     )
 
+def search(request):
+    search_term = request.GET.get('search_input')
+    if not search_term:
+        raise Http404()
+
+    p = Post.objects.filter(titulo__icontains=search_term).order_by("-created_at")
+    categorias = Categoria.objects.all()
+    posts = []
+    for post in p:
+        post_modificado = {
+            "titulo": post.titulo,
+            "descricao": post.descricao,
+            "likes": post.likes,
+            "imagem": post.imagem.url,
+            "blogueiro_id": post.blogueiro_id,
+            "created_at": post.created_at,
+            "categorias": post.categorias.all(),
+        }
+        posts.append(post_modificado)
+
+    try:
+        pagina_atual = int(request.GET.get("page", 1))
+    except ValueError:
+        pagina_atual = 1
+
+    paginator = Paginator(posts, 10)
+    posts_paginados = paginator.get_page(pagina_atual)
+    pagination_range = make_pagination_range(paginator.page_range, 10, pagina_atual)
+    return render(request,"posts/blog.html",context={"posts_paginados": posts_paginados,"links_pages": pagination_range,"categorias": categorias})
 
 def make_pagination_range(page_range, links_mostrados, qual_pagina_esta):
     metade_range = math.ceil(links_mostrados / 2)
@@ -72,7 +102,6 @@ def make_pagination_range(page_range, links_mostrados, qual_pagina_esta):
         "primeira_pagina_na_telah": qual_pagina_esta > metade_range,
         "ultima_pagina_na_telah": stop_range < total_page,
     }
-
 
 def pegar_numero(request, nome_campo) -> float:
     numero_string = request.POST.get(nome_campo)
